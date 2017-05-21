9bff041e73c832ba88378173d1f78594061782d9
Dan Waltin <dan@waltin.se>
2017-05-21 09:31:44 +0200

    added swift file template

---
+++ DW Swift File.xctemplate/Struct/___FILEBASENAME___.swift
@@ -0,0 +1,14 @@
+// ------------------------------------------------------------------------
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___FULLUSERNAME___
+//
+// ------------------------------------------------------------------------
+
+import Foundation
+
+struct ___FILEBASENAMEASIDENTIFIER___ {
+}
---
+++ DW Swift File.xctemplate/XCTest Case/___FILEBASENAME___.swift
@@ -0,0 +1,15 @@
+// ------------------------------------------------------------------------
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___FULLUSERNAME___
+//
+// ------------------------------------------------------------------------
+
+import XCTest
+@testable import ___PROJECTNAME___
+
+class ___FILEBASENAMEASIDENTIFIER___ : XCTestCase {
+}
